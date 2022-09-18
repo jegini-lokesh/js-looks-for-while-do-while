76bc7356baaee83bd2750c6625d46d3e1ce45a4d
jegini-lokesh <96949948+jegini-lokesh@users.noreply.github.com>
2022-09-18 23:25:31 +0530

    in this place i explain about while,for ,do while and also increment and decrement

---
+++ loop.js
@@ -0,0 +1,29 @@
+// var sum=0;
+// for (i = 1; i < 5; i++) {
+//     // document.write("welcome to learn java script"<br>) //how to break a line
+//     sum=sum+i;
+//     document.write(sum)
+// }
+//while loop 
+// var num=+prompt("enter a number")
+// while(num!=0) { //here loop will run upto satisiy the condition //if conition is true enter inside loop
+//     document.write("write correct number to satisisy condition"+ num + "<br>");
+//     var num=+prompt("enter a number")
+// }
+//do -while(here code is execute one time which is present in do while after then check condition)
+//  var count =5;
+//  do {
+//     var num=+prompt("enter a number")
+//     document.write("The entered number is"+ num + "<br>");
+//     count=count-1
+//  }while(count>0);
+ var a=10;
+//  var b=a++ //post increment (b=a,a=a+1) second step(a=11,b=10)
+ //var b=++a //pre increment (b=a,a=a+1) second step
+ //document.write("value of a is" + a);
+ //document.write("value of b is" + b);
+ //decrement operator
+   //var b=a-- //post increment (b=a,a=a+1) second step(b=10,a=9)
+ var b=--a //pre increment (b=a,a=a+1) second step(9,9)
+ document.write("value of a is" + a);
+ document.write("value of b is" + b);
